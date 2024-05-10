e3718c9b9e14c27da15877bb1ead081ef1cf1264
Alvin Ashiatey <alvinashiatey@gmail.com>
2024-05-10 12:18:49 -0400

    chore: Update npm dependencies and Astro configuration for Solid.js integration

---
+++ frontend/src/components/Counter.tsx
@@ -0,0 +1,8 @@
+import { createSignal, onCleanup } from "solid-js";
+
+export default function CountingComponent() {
+  const [count, setCount] = createSignal(0);
+  const interval = setInterval(() => setCount((c) => c + 1), 1000);
+  onCleanup(() => clearInterval(interval));
+  return <div>Count value is {count()}</div>;
+}
